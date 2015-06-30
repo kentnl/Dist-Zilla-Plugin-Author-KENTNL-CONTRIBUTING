9bc64dd7db82710de93a65d1da9e1a27c787f995
Kent Fredric <kentfredric@gmail.com>
2015-06-30 12:40:55 +1200

    v0.001001
     [Content]
     - Credit original author in emitted file.
    
     [Dependencies::Stats]
     - Dependencies changed since 0.001000, see misc/*.deps* for details
     - develop: +1 -1

--- lib/Dist/Zilla/Plugin/Author/KENTNL/CONTRIBUTING.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/CONTRIBUTING.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::Author::KENTNL::CONTRIBUTING;
 
-our $VERSION = '0.001001';
+our $VERSION = '0.001002';
 
 # ABSTRACT: Generates a CONTRIBUTING file for KENTNL's distributions.
 
