19c75f7cea817b8d1dbdc62dafe6e1815b245c0c
Kent Fredric <kentfredric@gmail.com>
2015-11-14 17:58:45 +1300

    v0.001005
     - switched from [GenerateFile::ShareDir] to [GenerateFile::FromShareDir], closes #1
    
     [Dependencies::Stats]
     - Dependencies changed since 0.001004, see misc/*.deps* for details
     - develop: +3 ↑1 -1 (suggests: ↑1)
     - runtime: +1 -3 (recommends: -1)

--- lib/Dist/Zilla/Plugin/Author/KENTNL/CONTRIBUTING.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/CONTRIBUTING.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::Author::KENTNL::CONTRIBUTING;
 
-our $VERSION = '0.001005';
+our $VERSION = '0.001006';
 
 # ABSTRACT: Generates a CONTRIBUTING file for KENTNL's distributions.
 
