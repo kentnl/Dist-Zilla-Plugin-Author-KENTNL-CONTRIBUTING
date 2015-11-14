187440bc74ef609b6a074b9715524ee550b5f695
Kent Fredric <kentfredric@gmail.com>
2015-11-14 17:58:53 +1300

    Build results of 19c75f7 (on master)

--- lib/Dist/Zilla/Plugin/Author/KENTNL/CONTRIBUTING.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/CONTRIBUTING.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::Author::KENTNL::CONTRIBUTING;
 
-our $VERSION = '0.001005';
+our $VERSION = '0.001006';
 
 # ABSTRACT: Generates a CONTRIBUTING file for KENTNL's distributions.
 
@@ -81,7 +81,7 @@ Dist::Zilla::Plugin::Author::KENTNL::CONTRIBUTING - Generates a CONTRIBUTING fil
 
 =head1 VERSION
 
-version 0.001005
+version 0.001006
 
 =head1 DESCRIPTION
 
