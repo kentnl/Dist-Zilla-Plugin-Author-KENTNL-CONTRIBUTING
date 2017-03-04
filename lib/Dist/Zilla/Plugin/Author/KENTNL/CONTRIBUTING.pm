3d8d18eb22d06f30738b81bb3d09a518efb2fe2f
Kent Fredric <kentfredric@gmail.com>
2017-03-04 14:37:48 +1300

    v0.001006
     [Bugfix]
     - Avoid test failures on 5.26 w/ -Ddefault_inc_excludes_dot
     - Closes https://github.com/kentnl/Dist-Zilla-Plugin-Author-KENTNL-CONTRIBUTING/issues/2
    
     [Dependencies::Stats]
     - Dependencies changed since 0.001005, see misc/*.deps* for details
     - develop: +1 ↑1 (suggests: ↑1)
     - runtime: -1
     - test: ↓1
    
     [Internals]
     - Drop ConfigDumper

--- lib/Dist/Zilla/Plugin/Author/KENTNL/CONTRIBUTING.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/CONTRIBUTING.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::Author::KENTNL::CONTRIBUTING;
 
-our $VERSION = '0.001006';
+our $VERSION = '0.001007';
 
 # ABSTRACT: Generates a CONTRIBUTING file for KENTNL's distributions.
 
