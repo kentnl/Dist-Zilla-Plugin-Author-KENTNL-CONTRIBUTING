8b30783f41febbf276bdc4823f3561a2d94df52b
Kent Fredric <kentfredric@gmail.com>
2017-03-04 14:24:23 +1300

    Build results of 542a826 (on master)

--- lib/Dist/Zilla/Plugin/Author/KENTNL/CONTRIBUTING.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/CONTRIBUTING.pm
@@ -122,7 +122,7 @@ Karen Etheridge <ether@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2015 by Kent Fredric <kentfredric@gmail.com>.
+This software is copyright (c) 2017 by Kent Fredric <kentfredric@gmail.com>.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
